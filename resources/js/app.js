d221116a9e226867c960b889fee93b7031aa7103
mmskazak <mmskazak@gmail.com>
2021-09-06 11:15:43 +0300

    install vuex

--- resources/js/app.js
+++ resources/js/app.js
@@ -1,6 +1,7 @@
 require('./bootstrap');
 
 import Vue from 'vue';
+import Vuex from 'vuex'
 import VueRouter from 'vue-router';
 import vuetify from './vuetify';
 
@@ -9,7 +10,20 @@ import routes from "./routes";
 
 console.log(routes);
 
+
+Vue.use(Vuex)
 Vue.use(VueRouter);
+
+//modules
+import a_module from "./modules/a-module";
+import b_module from "./modules/b-module";
+
+const store = new Vuex.Store({
+    modules: {
+        a_module,
+        b_module
+    }
+})
 
 
 const router = new VueRouter({
